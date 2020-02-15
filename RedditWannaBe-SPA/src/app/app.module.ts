df0aac895425d202f6c2cf9c55acdb3d4df696b5
Stefan Popoiu <stefan.popoiu@gmail.com>
2020-02-15 10:17:32 +0200

    Setup SPA

--- RedditWannaBe-SPA/src/app/app.module.ts
+++ RedditWannaBe-SPA/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      FooterComponent,
+      HeaderComponent,
+      NotFoundComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
