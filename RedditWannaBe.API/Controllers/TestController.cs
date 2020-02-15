527b89666a67287fc51bb52058b443f9545aecb4
Stefan Popoiu <stefan.popoiu@gmail.com>
2020-02-15 09:59:09 +0200

    Setup the API

---
+++ RedditWannaBe.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedditWannaBe.API.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
