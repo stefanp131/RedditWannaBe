4f36fb7fe1fb98600943746b8340d227fc85a9d7
Stefan Popoiu <stefan.popoiu@gmail.com>
2020-02-18 14:00:38 +0200

    move folders

--- RedditWannaBe-SPA/src/app/app.module.ts
+++ RedditWannaBe-SPA/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from '../home/home.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { LoginComponent } from '../login/login.component';
 
 @NgModule({
    declarations: [
